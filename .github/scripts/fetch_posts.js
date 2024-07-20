c9d4d9f9456bc60984c148e7e50049bd245b4203
gwagjiug <kwjo0228@naver.com>
2024-07-21 00:03:46 +0900

    수정

--- .github/scripts/fetch_posts.js
+++ .github/scripts/fetch_posts.js
@@ -7,13 +7,17 @@ async function fetchPosts() {
     const response = await axios.get('https://api.velog.io/rss/@gawgjiug');
     const result = await xml2js.parseStringPromise(response.data);
     const items = result.rss.channel[0].item;
-    const posts = items.map(item => ({
-      title: item.title[0],
-      link: item.link[0],
-      description: item.description[0],
-      pubDate: item.pubDate[0],
-      thumbnail: item['media:content'] ? item['media:content'][0].$.url : null
-    })).slice(0, 6); // 최신 6개 포스트만 저장
+    const posts = items
+      .map((item) => ({
+        title: item.title[0],
+        link: item.link[0],
+        description: item.description[0],
+        pubDate: item.pubDate[0],
+        thumbnail: item['media:content']
+          ? item['media:content'][0].$.url
+          : null,
+      }))
+      .slice(0, 6); // 최신 6개 포스트만 저장
     fs.writeFileSync('posts.json', JSON.stringify(posts, null, 2));
   } catch (error) {
     console.error('Error fetching posts:', error);
